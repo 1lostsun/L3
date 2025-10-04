e100ef96afea5026e7814b4d3a5b90b13532abcf
1lostsun <149478863+1lostsun@users.noreply.github.com>
2025-10-04 13:39:21 +0300

    empty l3_2

---
+++ L3_2/cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	s := randomString(5)
+	fmt.Println(s)
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
